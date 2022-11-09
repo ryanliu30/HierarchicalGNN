b781eadbbf9b84c0a019e89e957b26c89c2ed8da
ryanliu30 <liuryan30@gmail.com>
2022-11-08 21:12:33 -0800

    updated hyperparameter

--- Modules/gnn_utils.py
+++ Modules/gnn_utils.py
@@ -11,6 +11,9 @@ import cupy as cp
 sys.path.append("..")
 from utils import make_mlp, find_neighbors
 
+def checkpointing(func):
+    return lambda *x: checkpoint(func, *x)
+
 class InteractionGNNCell(nn.Module):
     def __init__(self, hparams):
         super().__init__()
@@ -38,20 +41,33 @@ class InteractionGNNCell(nn.Module):
         )
         
         self.hparams = hparams
-        
-    def forward(self, nodes, edges, graph):
-        
-        # Compute new node features
+    
+    @checkpointing
+    def node_update(self, nodes, edges, graph):
+        """
+        Calculate node update with checkpointing
+        """
         edge_messages = scatter_add(edges, graph[1], dim=0, dim_size=nodes.shape[0])
         node_input = torch.cat([nodes, edge_messages], dim=-1)
-        nodes = checkpoint(self.node_network, node_input) + nodes # Skip connection
-        del node_input, edge_messages
-            
-        # Compute new edge features
-        edge_input = torch.cat([nodes[graph[0]], nodes[graph[1]], edges], dim=-1)
-        edges = checkpoint(self.edge_network, edge_input) + edges # Skip connection
-        del edge_input
+        nodes = self.node_network(node_input) + nodes # Skip connection
         
+        return nodes
+    
+    @checkpointing
+    def edge_update(self, nodes, edges, graph):
+        """
+        Calculate edge update with checkpointing
+        """
+        edge_input = torch.cat([nodes[graph[0]], nodes[graph[1]], edges], dim=-1)
+        edges = self.edge_network(edge_input) + edges # Skip connection
+        
+        return edges        
+    
+    def forward(self, nodes, edges, graph):
+
+        nodes = self.node_update(nodes, edges, graph)
+        edges = self.edge_update(nodes, edges, graph)
+
         return nodes, edges
 
 class HierarchicalGNNCell(nn.Module):
@@ -99,28 +115,56 @@ class HierarchicalGNNCell(nn.Module):
         )
         
         self.hparams = hparams
-        
+    
+    @checkpointing 
+    def node_update(self, nodes, edges, supernodes, graph, bipartite_graph, bipartite_edge_weights):
+        """
+        Calculate node updates with checkpointing
+        """
+        supernode_messages = scatter_add(bipartite_edge_weights*supernodes[bipartite_graph[1]], bipartite_graph[0], dim=0, dim_size=nodes.shape[0])
+        edge_messages = scatter_add(edges, graph[1], dim=0, dim_size=nodes.shape[0])
+        nodes = self.node_network(torch.cat([nodes, edge_messages, supernode_messages], dim=-1)) + nodes
+        return nodes
+    
+    @checkpointing 
+    def edge_update(self, nodes, edges, graph):
+        """
+        Calculate edge updates with checkpointing
+        """
+        edges = self.edge_network(torch.cat([nodes[graph[0]], nodes[graph[1]], edges], dim=-1)) + edges
+        return edges
+    
+    @checkpointing 
+    def supernode_update(self, nodes, supernodes, superedges, bipartite_graph, bipartite_edge_weights, super_graph, super_edge_weights):
+        """
+        Calculate supernode updates with checkpointing
+        """
+        node_messages = scatter_add(bipartite_edge_weights*nodes[bipartite_graph[0]], bipartite_graph[1], dim=0, dim_size=supernodes.shape[0])
+        attention_messages = scatter_add(superedges*super_edge_weights, super_graph[1], dim=0, dim_size=supernodes.shape[0])
+        supernodes = self.supernode_network(torch.cat([supernodes, attention_messages, node_messages], dim=-1)) + supernodes
+        return supernodes
+    
+    @checkpointing
+    def superedge_update(self, supernodes, superedges, super_graph, super_edge_weights):
+        """
+        Calculate superedge updates with checkpointing
+        """
+        superedges = self.superedge_network(torch.cat([supernodes[super_graph[0]], supernodes[super_graph[1]], superedges], dim=-1)) + superedges
+        return superedges
+       
     def forward(self, nodes, edges, supernodes, superedges, graph, bipartite_graph, bipartite_edge_weights, super_graph, super_edge_weights):
         """
         Whereas the message passing in the original/super graphs is implemented by interaction network, the one in between them (bipartite message 
         passing is descirbed by weighted graph convolution (vanilla aggregation without attention)
         """
         
-        # Compute new supernode features
-        node_messages = scatter_add(bipartite_edge_weights*nodes[bipartite_graph[0]], bipartite_graph[1], dim=0, dim_size=supernodes.shape[0])
-        attention_messages = scatter_add(superedges*super_edge_weights, super_graph[1], dim=0, dim_size=supernodes.shape[0])
-        supernodes = checkpoint(self.supernode_network, torch.cat([supernodes, attention_messages, node_messages], dim=-1)) + supernodes
-        del node_messages, attention_messages
-        
-        # Compute original graph updates
-        supernode_messages = scatter_add(bipartite_edge_weights*supernodes[bipartite_graph[1]], bipartite_graph[0], dim=0, dim_size=nodes.shape[0])
-        edge_messages = scatter_add(edges, graph[1], dim=0, dim_size=nodes.shape[0])
-        nodes = checkpoint(self.node_network, torch.cat([nodes, edge_messages, supernode_messages], dim=-1)) + nodes
-        del supernode_messages, edge_messages
+        # Compute new node features
+        supernodes = self.supernode_update(nodes, supernodes, superedges, bipartite_graph, bipartite_edge_weights, super_graph, super_edge_weights)
+        nodes = self.node_update(nodes, edges, supernodes, graph, bipartite_graph, bipartite_edge_weights)
         
         # Compute new edge features
-        superedges = checkpoint(self.superedge_network, torch.cat([supernodes[super_graph[0]], supernodes[super_graph[1]], superedges], dim=-1)) + superedges
-        edges = checkpoint(self.edge_network, torch.cat([nodes[graph[0]], nodes[graph[1]], edges], dim=-1)) + edges
+        superedges = self.superedge_update(supernodes, superedges, super_graph, super_edge_weights)
+        edges = self.edge_update(nodes, edges, graph)
         
         return nodes, edges, supernodes, superedges
 
@@ -166,7 +210,7 @@ class DynamicGraphConstruction(nn.Module):
         edge_weights = self.weighting_function(edge_weights_logits)
         
         if norm:
-            edge_weights = edge_weights/(1e-12 + scatter_add(edge_weights, graph[0], dim=0, dim_size = src_embeddings.shape[0])[graph[0]])
+            edge_weights = edge_weights/edge_weights.mean()
         edge_weights = edge_weights.unsqueeze(1)
         if logits:
             return graph, edge_weights, edge_weights_logits
